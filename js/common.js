899758a9da5389173b2e99f285b6cf45d5cac48d
leuyrem <leuyren@gmail.com>
2024-02-28 17:37:06 +0900

    5

--- js/common.js
+++ js/common.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
-  $("#hamburger").click(function () {
-      $(this).toggleClass("is-active");
+    $("#hamburger").click(function () {
+        $(this).toggleClass("is-active");
 
-      if($('#menu_wrap').hasClass('full_n')){
-          $('#menu_wrap>ul').hide();
-      } else {
-          $('#menu_wrap>ul').fadeIn(1000);
-      }
+        if ($('#menu_wrap').hasClass('full_n')) {
+            $('#menu_wrap>ul').hide();
+        } else {
+            $('#menu_wrap>ul').fadeIn(1000);
+        }
 
-      $('#menu_wrap').toggleClass('full_n');
-  });
+        $('#menu_wrap').toggleClass('full_n');
+    });
 });
+
+
+// 헤더 색상변경
+// $(window).scroll(function () {
+//     var height = $(this).scrollTop();
+//     if (height > 300 && height < 1200) {
+//         $('.logo').attr("src", "img/logo(black_03).png");
+//         $('#hamburger .line').css("bakground-color", "#000");
+//     }
+
+
+// });
--- js/sub_artist.js
+++ js/sub_artist.js
@@ -1,31 +1,199 @@
 // 서브메뉴 색상변경
-$(window).scroll(function(){
+$(window).scroll(function () {
     var height = $(this).scrollTop();
-    if(height > 700 &&  height<=2600) {
-        $('.topbar li a:hover').css("color","#7afafa");
-        $('#home .topbar li a').css("color","#000");
-    }else if(height <600 || height >2600){
-        $('.topbar li a:hover').css("color","#7afafa");
-        $('#home .topbar li a').css("color","#fff");
-  
+    if (height < 600 || height > 8500) {
+        $('.topbar li a').hover(function(){
+            $(this).css("color", "#7afafa")
+        },function(){
+            $(this).css("color", "#fff")
+        });
+        $('.topbar li a').css("color", "#fff");
+
+    } else if (height > 300 && height < 2000) {
+        $('.topbar li a').hover(function(){
+            $(this).css("color", "#7afafa")
+        },function(){
+            $(this).css("color", "#000")
+        });
+        $('.topbar li a').css("color", "#000");
+
+    } else if (height > 2200 && height < 3000) {
+        $('.topbar li a').hover(function(){
+            $(this).css("color", "#7afafa")
+        },function(){
+            $(this).css("color", "#fff")
+        }); 
+        $('.topbar li a').css("color", "#fff");
+
+    } else if (height > 2000 && height < 6300) {
+        $('.topbar li a').hover(function(){
+            $(this).css("color", "#7afafa")
+        },function(){
+            $(this).css("color", "#000")
+        });        
+        $('.topbar li a').css("color", "#000");
+
+    } else if (height > 6300) {
+        $('.topbar li a').hover(function(){
+            $(this).css("color", "#7afafa")
+        },function(){
+            $(this).css("color", "#fff")
+        });         
+        $('.topbar li a').css("color", "#fff");
     }
 });
 
 
 
-// $(window).scroll(function(){ 
-//     var height = $(document).scrollTop(); 
-//     if(height > 850){ 
+// scrollY
+// window.addEventListener('scroll', function(){
+//     console.log(window.scrollY)
+// });
+
+
+//앨범 슬라이드
+window.onload=function(){
+const sliderWrap = document.querySelector(".slider__wrap");
+const sliderImg = sliderWrap.querySelector(".slider__img");
+const sliderInner = sliderWrap.querySelector(".slider__inner");
+const slider = sliderWrap.querySelectorAll(".slider");
+const sliderDot = sliderWrap.querySelector(".slider__dot");
+const sliderBtn = sliderWrap.querySelectorAll(".slider__btn a");
+
+let currentIndex = 0;                   //현재 보이는 (가장 위에 있는) 이미지
+let sliderInterval = 3000;              //다음 이미지로 넘어가는 간격의 시간
+let sliderCount = slider.length;        //전체 이미지의 총 수
+let sliderWidth = sliderWrap.querySelector(".slider__img").offsetWidth;            //이미지 width 값
+let dotIndex = "";
+
+function init() {
+    //슬라이드 될 이미지만큼 도트 메뉴 생성
+    slider.forEach(() => dotIndex += "<a href='#' class='dot'></a>");
+    sliderDot.innerHTML = dotIndex;
+
+    //도트 메뉴 활성화 (첫 번째)
+    sliderDot.firstChild.classList.add("active");
+}
+init();
+
+//슬라이드 이미지의 위치 변경
+function moveSlider(num) {
+    sliderInner.style.transition = "all 400ms";
+    sliderInner.style.transform = `translateX(-${sliderWidth * num}px)`;
+    currentIndex = num;
+
+
+    //버튼 활성화
+    let DotActive = document.querySelectorAll(".slider__dot .dot");
+    DotActive.forEach((active) => active.classList.remove("active"));
+    DotActive[num].classList.add("active");
+}
+
+// 도트 메뉴 클릭 시 이동
+sliderDot.querySelectorAll(".dot").forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        moveSlider(index);
+    });
+});
+
+//슬라이드 버튼 클릭 시
+sliderBtn.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        let previousIndex = (currentIndex + (sliderCount - 1)) % sliderCount;
+        let nextIndex = (currentIndex + 1) % sliderCount;
+
+        if (btn.classList.contains("previous")) {
+            moveSlider(previousIndex);
+        } else {
+            moveSlider(nextIndex);
+        }
+    });
+});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let indexA =1;
+// function ALBUM(disco5, disco4, disco3, disco2, disco1) {
+//     this.disco5=disco5;
+//     this.disco4=disco4;
+//     this.disco3=disco3;
+//     this.disco2=disco2;
+//     this.disco1=disco1;
+// }
+
+// ALBUM.prototype.printImg =function(){
+//     picArea.setAttribute("src",this.disco5);
+// }
+
+// const ALBUM1 = new ALBUM("sub_img/disco5.jpg")
+// const ALBUM2 = new ALBUM("sub_img/disco4.jpg")
+// const ALBUM3 = new ALBUM("sub_img/disco3.jpg")
+// const ALBUM4 = new ALBUM("sub_img/disco2.jpg")
+// const ALBUM5 = new ALBUM("sub_img/disco1.jpg")
+
+// this.onclick =function(e) {
+//     if(e.target.id =="prev" || e.target.id=="next"){
+//         if(e.target.id=="prev"){indexA--;}
+//         if(e.target.id=="next"){indexA++;}
+
+//         switch (indexA) {
+//             case 0: ALBUM1.printImg(); break;
+//             case 1: ALBUM2.printImg(); break;
+//             case 2: ALBUM3.printImg(); break;
+//             case 3: ALBUM4.printImg(); break;
+//             case 4: ALBUM5.printImg(); break;            
+//         }
+//     }
+// }
+
+
+
+
+// $(window).scroll(function(){
+//     var height = $(document).scrollTop();
+//     if(height > 850){
 //         $(".surch").attr("src", "./img/icon_black.png");
 //         $(".logoimg").attr("src", "./img/logo_black.png");
 //         $(".login").css("color","#000")
 //         $(".line").css("backgroundColor","#000")
-//     }else if(height < 850){ 
+//     }else if(height < 850){
 //         $(".surch").attr("src", "./img/surch.png");
 //         $(".logoimg").attr("src", "./img/logo1.png");
 //         $(".login").css("color","#fff")
 //         $(".line").css("backgroundColor","#fff")
-//     } 
+//     }
 //   });
 
 
